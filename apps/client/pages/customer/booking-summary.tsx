317cb162e6e6c392383863deef15a939cca33995
braingrams <66751512+braingrams@users.noreply.github.com>
2023-06-13 06:16:35 +0100

    Fixed build

--- apps/client/pages/customer/booking-summary.tsx
+++
@@ -1,8 +0,0 @@
-import BookingSummary from "@components/bookingSummary/BookingSummary";
-import React from "react";
-
-const summary = () => {
-    return <BookingSummary />;
-};
-
-export default summary;
